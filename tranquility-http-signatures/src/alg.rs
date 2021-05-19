6f2aabfdce4fe4fe42964f328086d074fc295b7e
Glitch <smallglitch@cryptolab.net>
2021-05-19 18:00:17 +0200

    add comments, add default impl

--- tranquility-http-signatures/src/alg.rs
+++ tranquility-http-signatures/src/alg.rs
@@ -2,7 +2,7 @@ use {
     crate::error::{Error, Result},
     ring::{
         rand,
-        signature::{RsaKeyPair, UnparsedPublicKey},
+        signature::{RsaKeyPair, UnparsedPublicKey, VerificationAlgorithm},
     },
 };
 
@@ -18,12 +18,21 @@ use ring::signature::{
     RSA_PKCS1_SHA256, RSA_PKCS1_SHA384, RSA_PKCS1_SHA512,
 };
 
+/// Struct holding information about the algorithms used to create the signature
+///
+/// Defaults to `rsa-sha256`
 pub struct Algorithm<'a> {
     /// Cryptographic algorithm being used (eg. `rsa`, `ecdsa`, `hmac` etc.)
     crypto: &'a str,
 
     /// Hash algorithm being used (eg. `sha256`, `sha384`, etc.)
     hash: &'a str,
+}
+
+impl<'a> Default for Algorithm<'a> {
+    fn default() -> Self {
+        ("rsa", "sha256").into()
+    }
 }
 
 impl<'a> From<(&'a str, &'a str)> for Algorithm<'a> {
@@ -40,6 +49,8 @@ impl<'a> ToString for Algorithm<'a> {
 
 impl<'a> Algorithm<'a> {
     /// Parse the value of the algorithm field of the signature header
+    ///
+    /// `Option::None` will return the default algorithm
     pub fn parse(raw_str: Option<&'a str>) -> Result<Self> {
         if let Some(raw_str) = raw_str {
             let (crypto, hash) =
@@ -50,8 +61,8 @@ impl<'a> Algorithm<'a> {
 
             Ok((crypto, hash).into())
         } else {
-            // Default to RSA-SHA256
-            Ok(("rsa", "sha256").into())
+            // Assume that the default algorithm applies
+            Ok(Self::default())
         }
     }
 
@@ -60,22 +71,7 @@ impl<'a> Algorithm<'a> {
     where
         K: AsRef<[u8]>,
     {
-        #[cfg(not(test))]
-        let algorithm = match (self.crypto, self.hash) {
-            ("rsa", "sha256") => &RSA_PKCS1_2048_8192_SHA256,
-            ("rsa", "sha384") => &RSA_PKCS1_2048_8192_SHA384,
-            ("rsa", "sha512") => &RSA_PKCS1_2048_8192_SHA512,
-
-            _ => return Err(Error::UnknownKeyType),
-        };
-
-        #[cfg(test)]
-        let algorithm = match (self.crypto, self.hash) {
-            ("rsa", "sha256") => &RSA_PKCS1_1024_8192_SHA256_FOR_LEGACY_USE_ONLY,
-            ("rsa", "sha512") => &RSA_PKCS1_1024_8192_SHA512_FOR_LEGACY_USE_ONLY,
-
-            _ => unreachable!(),
-        };
+        let algorithm = get_algorithm(self.crypto, self.hash)?;
 
         Ok(UnparsedPublicKey::new(algorithm, key_bytes))
     }
@@ -106,4 +102,27 @@ impl<'a> Algorithm<'a> {
             Err(Error::UnknownAlgorithm)
         }
     }
+}
+
+/// Get the ring algorithm from the HTTP signatures crypto and hash algorithm identifier
+fn get_algorithm(crypto: &str, hash: &str) -> Result<&'static dyn VerificationAlgorithm> {
+    #[cfg(not(test))]
+    let algorithm = match (crypto, hash) {
+        ("rsa", "sha256") => &RSA_PKCS1_2048_8192_SHA256,
+        ("rsa", "sha384") => &RSA_PKCS1_2048_8192_SHA384,
+        ("rsa", "sha512") => &RSA_PKCS1_2048_8192_SHA512,
+
+        _ => return Err(Error::UnknownKeyType),
+    };
+
+    // Enable unsecure key lengths for the tests because the official RFC examples are created using an RSA-1024 bit key
+    #[cfg(test)]
+    let algorithm = match (crypto, hash) {
+        ("rsa", "sha256") => &RSA_PKCS1_1024_8192_SHA256_FOR_LEGACY_USE_ONLY,
+        ("rsa", "sha512") => &RSA_PKCS1_1024_8192_SHA512_FOR_LEGACY_USE_ONLY,
+
+        _ => unreachable!(),
+    };
+
+    Ok(algorithm)
 }
