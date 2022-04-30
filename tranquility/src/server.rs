e06d0edba7568434b764ab24ab958e7a7c77c6bf
aumetra <aumetra@cryptolab.net>
2022-04-30 21:10:05 +0200

    extract router creation into own function

--- tranquility/src/server.rs
+++ tranquility/src/server.rs
@@ -1,5 +1,5 @@
 use crate::state::ArcState;
-use axum::{Extension, Router};
+use axum::{extract::connect_info::IntoMakeServiceWithConnectInfo, Extension, Router};
 use axum_server::tls_rustls::RustlsConfig;
 use std::{
     io,
@@ -8,27 +8,31 @@ use std::{
 };
 use tower_http::{compression::CompressionLayer, trace::TraceLayer};
 
-/// Combine all route filters and start a warp server
-pub async fn run(state: ArcState) -> io::Result<()> {
+/// Construct the combined router
+pub fn create_router_make_service(
+    state: &ArcState,
+) -> IntoMakeServiceWithConnectInfo<Router, SocketAddr> {
     let router = Router::new()
         .merge(crate::activitypub::routes())
-        .merge(crate::api::routes(&state))
+        .merge(crate::api::routes(state))
         .merge(crate::well_known::routes())
-        .layer(Extension(Arc::clone(&state)))
+        .layer(Extension(Arc::clone(state)))
         .layer(TraceLayer::new_for_http())
         .layer(CompressionLayer::new());
 
     #[cfg(feature = "email")]
     let router = router.merge(crate::email::routes());
 
-    let router_service = router.into_make_service_with_connect_info::<SocketAddr>();
+    router.into_make_service_with_connect_info()
+}
 
-    let config = &state.config;
+/// Combine all routers and start the webserver
+pub async fn run(state: ArcState) -> io::Result<()> {
+    let router_service = create_router_make_service(&state);
+    let interface = state.config.server.interface.parse::<IpAddr>().unwrap();
+    let addr = (interface, state.config.server.port);
 
-    let interface = config.server.interface.parse::<IpAddr>().unwrap();
-    let addr = (interface, config.server.port);
-
-    if config.tls.serve_tls_directly {
+    if state.config.tls.serve_tls_directly {
         let config = RustlsConfig::from_pem_file(
             &state.config.tls.certificate,
             &state.config.tls.secret_key,
